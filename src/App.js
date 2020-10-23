d843b37354de052df49e0eeed8b9e3a3090e3c20
Christophe Collot <christophe.collot.pro@gmail.com>
2020-10-23 09:21:12 +0200

    add feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>aasrc/App.js</code> and save sdsdto reload haha this  app.
+          Edit <code>feature/App.js</code> and save and reload this app from.
         </p>
         <a
           className="App-link"
